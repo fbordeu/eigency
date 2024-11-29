6d096819d442eb9738d25ee17007b5ae2217fd25
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2024-11-29 11:25:27 +0100

    Revert "remove deprecated types"
    This reverts commit be9d1d582ea9f0ac46bfa31c1f44f3551b163df2.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -167,13 +167,13 @@ class TestEigency(unittest.TestCase):
 
     def test_function_type_long(self):
         # C++ long - Note that this is the standard Python integer
-        mat_in = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], order="F", dtype=np.int32)
+        mat_in = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], order="F", dtype=np.long)
         mat_out = eigency_tests.function_type_long(mat_in)
         assert_array_equal(mat_in, mat_out)
 
     def test_function_type_ulong(self):
         # C++ ulong
-        mat_in = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], order="F", dtype=np.uint32)
+        mat_in = np.array([[1, 2, 3, 4], [5, 6, 7, 8]], order="F", dtype=np.ulong)
         mat_out = eigency_tests.function_type_ulong(mat_in)
         assert_array_equal(mat_in, mat_out)
 
