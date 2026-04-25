6e09a4230f07b7b0e811c0863d25e7203a399d7a
Felipe Bordeu <felipebordeu@gmail.com>
2026-04-25 22:54:47 +0930

    add support for external EIGEN_INC in the tests

--- tests/setup.py
+++ tests/setup.py
@@ -2,14 +2,28 @@ import numpy as np
 from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
+from os.path import basename, join
+import os
 
 import eigency
+
+include_dirs = [np.get_include()]
+if "EIGEN_INC" in os.environ:
+    useSystemEigen = True
+    include_dirs.append(os.environ["EIGEN_INC"])
+else:
+    useSystemEigen = False
+    import eigency  # noqa: E402
+
+    __eigen_dir__ = eigency.get_eigency_eigen_dir()
+    __eigen_lib_dir__ = join(basename(__eigen_dir__), "Eigen")
+    include_dirs.append(__eigen_dir__)
 
 extensions = [
     Extension(
         "eigency_tests.eigency_tests",
         sources=["eigency_tests/eigency_tests.pyx"],
-        include_dirs=[".", "eigency_tests"] + eigency.get_includes(),
+        include_dirs=[".", "eigency_tests"] + include_dirs,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
 ]
@@ -19,7 +33,7 @@ if np.lib.NumpyVersion(np.__version__) >= "2.0.0":
         Extension(
             "eigency_tests.eigency_np2",
             sources=["eigency_tests/eigency_np2.pyx"],
-            include_dirs=[".", "eigency_tests"] + eigency.get_includes(),
+            include_dirs=[".", "eigency_tests"] + include_dirs,
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         )
     )
